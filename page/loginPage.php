d7d51269d19e571274eadb71fc1d93fee192f8e9
johanYW <johanwahyu49@gmail.com>
2022-10-15 20:39:06 +0700

    Revisi di peminjamanPage

--- page/loginPage.php
+++ page/loginPage.php
@@ -15,7 +15,7 @@
         <nav class="navbar navbar-dark bg-dark fixed-top">
         <div class="container">
             <!-- Nama : nama panggilan kalian -->
-            <a class="navbar-brand fw-bold" href="/gd4_x_yyyy">PERPUSTAKAAN KEL E</a>
+            <a class="navbar-brand fw-bold" href="../index.php">PERPUSTAKAAN KEL E</a>
         </div>
             </nav>
             <div class="bg bg-light text-dark">
